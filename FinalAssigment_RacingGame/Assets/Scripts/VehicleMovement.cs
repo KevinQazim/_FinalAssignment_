64ed8b7a683477f841a4a2867efdd3b8a57f945e
Aidan Feser <hkfan8@gmail.com>
2021-06-03 10:36:48 -0700

    commited to the booba

---
+++ FinalAssigment_RacingGame/Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovement : MonoBehaviour
+{
+    
+
+    private float xSpeed = 10f;
+    private float Power = 1f;
+    private float Friction = 0.1f;
+    private bool Right = false;
+    private bool left = false;
+
+
+    private void FixedUpdate()
+    {
+        if (Right)
+        {
+            xSpeed += Power;
+        }
+        if (left)
+        {
+            xSpeed -= Power;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Right = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Right = false;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            left = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            left = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+
+        xSpeed *= Friction;
+        transform.Translate(Vector3.right * -xSpeed);
+    }
+}
