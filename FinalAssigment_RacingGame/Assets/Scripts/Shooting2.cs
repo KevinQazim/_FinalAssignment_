883114c2c1f01cc1155cff415cd92c5653c56972
IRBerryz <austinbarber94@gmail.com>
2021-06-12 21:06:16 -0700

    shooting corrected

--- FinalAssigment_RacingGame/Assets/Scripts/Shooting2.cs
+++ FinalAssigment_RacingGame/Assets/Scripts/Shooting2.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooting : MonoBehaviour
+public class Shooting2 : MonoBehaviour
 {
-    public GameObject bullet;
-    public GameObject bullet_emitter;
-    public float speed = 100f;
+    public GameObject bullet2;
+    public GameObject bullet_emitter2;
+    public float speed2 = 100f;
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.RightControl))
         {
             GameObject temporaryBulletHandler;
-            temporaryBulletHandler = Instantiate(bullet,bullet_emitter.transform.position,bullet_emitter.transform.rotation) as GameObject;
+            temporaryBulletHandler = Instantiate(bullet2,bullet_emitter2.transform.position,bullet_emitter2.transform.rotation) as GameObject;
             temporaryBulletHandler.transform.Rotate(Vector3.left * 90);
             Rigidbody temporaryRigidBody;
             temporaryRigidBody = temporaryBulletHandler.GetComponent<Rigidbody>();
-            temporaryRigidBody.AddForce(transform.forward * speed);
+            temporaryRigidBody.AddForce(transform.forward * speed2);
             Destroy(temporaryBulletHandler, 3.0f);
         }  
     }
