b5959de67fd4c20e415ad0f2fa924e011a9a7d59
Kevin Qazim <kevinqazim@gmail.com>
2021-06-15 10:57:37 -0700

    Speed Boosts

---
+++ FinalAssigment_RacingGame/Assets/Scripts/SpeedBoostScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoostScript : MonoBehaviour
+{
+    public VehicleMovement player1Speed;
+    public VechicleMovementPlayer2 player2Speed;
+
+    public float boostDurationCar1;
+    public float boostTimeCar1;
+
+    public float boostDurationCar2;
+    public float boostTimeCar2;
+
+    // Start is called before the first frame update
+
+    public void Start()
+    {
+        boostTimeCar1 = 0;
+
+        boostTimeCar2 = 0;
+    }
+
+    public void Update()
+    {
+        boostTimeCar1 += Time.deltaTime;
+
+        boostTimeCar2 += Time.deltaTime;
+
+        if (boostTimeCar1 >= boostDurationCar1)
+        {
+            player1Speed.GetComponent<VehicleMovement>().Power = 1.5f;
+
+            boostTimeCar1 = 0;
+        }
+
+        if (boostTimeCar2 >= boostDurationCar2)
+        {
+            player2Speed.GetComponent<VechicleMovementPlayer2>().Power = 1.5f;
+
+            boostTimeCar2 = 0;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player1")
+        {
+            boostTimeCar1 = 0;
+
+            player1Speed.GetComponent<VehicleMovement>().Power = 2f;
+
+        }
+
+        if (other.gameObject.tag == "player2")
+        {
+            boostTimeCar2 = 0;
+
+            player2Speed.GetComponent<VechicleMovementPlayer2>().Power = 2f;
+        }
+    }
+}
